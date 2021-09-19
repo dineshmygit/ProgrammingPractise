42297441e47707e69ec7c6ba85c5eb6a81f46c07
dineshmygit <51089721+dineshmygit@users.noreply.github.com>
2021-09-19 17:31:05 +0530

    Update HelloWorld.java

--- SDETtraning/src/main/java/InterviewPreparation/HelloWorld.java
+++ SDETtraning/src/main/java/InterviewPreparation/HelloWorld.java
@@ -2,7 +2,7 @@ package InterviewPreparation;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hi jenkins found the file");
+		System.out.println("Hi jenkins found the file   ");
 	}
 
 }
